1b4ec4ee854795ceea777fbd74bdc12fe47250ae
XzwHan <xizewen.han@utexas.edu>
2022-11-16 22:08:43 +0000

    added training scripts for regression toy examples

--- regression/training_scripts/run_toy_quadratic_regression.sh
+++ regression/training_scripts/run_toy_quadratic_regression.sh
@@ -1,14 +1,14 @@
 export EXP_DIR=./results
 export N_STEPS=1000
 export SERVER_NAME=a4000
-export RUN_NAME=run_2
+export RUN_NAME=run_1
 export LOSS=card_conditional
-export TASK=uci_naval
-export N_SPLITS=20
+export TASK=toy_quadratic_regression
+export N_SPLITS=10
 export N_THREADS=4
-export DEVICE_ID=3
+export DEVICE_ID=2
 
 export CAT_F_PHI=_cat_f_phi
-export MODEL_VERSION_DIR=card_conditional_uci_results/${N_STEPS}steps/nn/${RUN_NAME}_${SERVER_NAME}/f_phi_prior${CAT_F_PHI}
+export MODEL_VERSION_DIR=card_conditional_toy_results/${N_STEPS}steps/nn/${RUN_NAME}_${SERVER_NAME}/f_phi_prior${CAT_F_PHI}
 python main.py --device ${DEVICE_ID} --thread ${N_THREADS} --loss ${LOSS} --exp $EXP_DIR/${MODEL_VERSION_DIR} --run_all --n_splits ${N_SPLITS} --doc ${TASK} --config configs/${TASK}.yml #--train_guidance_only
 python main.py --device ${DEVICE_ID} --thread ${N_THREADS} --loss ${LOSS} --exp $EXP_DIR/${MODEL_VERSION_DIR} --run_all --n_splits ${N_SPLITS} --doc ${TASK} --config $EXP_DIR/${MODEL_VERSION_DIR}/logs/ --test
